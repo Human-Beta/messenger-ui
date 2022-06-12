d4a9f532f06292d5ed8f33785c0d07b03f1f91ff
Nikita Shishov <workmailnikita@gmail.com>
2022-06-12 17:24:32 +0300

    client: add redux and sass libraries, create emtpty redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
