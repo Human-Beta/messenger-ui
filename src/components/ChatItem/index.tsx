a55f4afa0d286e892f199adcc992070b980cfe83
Nikita Shishov <workmailnikita@gmail.com>
2022-08-18 12:39:29 +0300

    move getDateString function to the utils

--- src/components/ChatItem/index.tsx
+++ src/components/ChatItem/index.tsx
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -7,25 +6,10 @@ import { getSelectedChat } from '../../redux/chat/selectors';
 import { Link } from 'react-router-dom';
 import avatarSvg from '../../assets/img/avatar.svg';
 import { getLastMessage } from '../../redux/message/selectors';
+import { getDateString } from '../../utils';
 import styles from './ChatItem.module.scss';
 
 const { sender, message, time, active } = styles;
-
-// TODO: move somewhere (utils?)
-const getDateString = (date: string): string => {
-  const dateMoment = moment(date);
-  const now = moment();
-
-  if (dateMoment.isBefore(now, 'year')) {
-    return dateMoment.format('DD.MM.YY');
-  } else if (dateMoment.isBefore(now, 'month')) {
-    return dateMoment.format('MMMM Do');
-  } else if (dateMoment.isBefore(now, 'day')) {
-    return dateMoment.format('ddd');
-  }
-
-  return dateMoment.format('HH:mm');
-};
 
 interface ChatItemProps {
   chat: Chat;
