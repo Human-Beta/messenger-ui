43104f361ceda1416daf32594433a83886687f64
Nikita Shishov <workmailnikita@gmail.com>
2023-02-05 00:45:07 +0200

    CI/CD: fix cicd.yml file and add serverless.yml file

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@
 // TODO: profile in react
 const config = {
   local: {
-    HOST_URL: 'http://localhost:8080',
+    // TODO: change back
+    HOST_URL: 'http://3.212.223.26',
     ACCESS_TOKEN_KEY: 'accessToken',
     BASIC_AUTH_HEADER: 'dGVzdF9pZDp0ZXN0X3NlY3JldA==',
   },
